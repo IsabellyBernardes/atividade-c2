b57f5e77fdd76e8d60682791c3329b4cd2308d98
IsabellyBernardes <113481194+IsabellyBernardes@users.noreply.github.com>
2024-12-05 14:47:01 +0000

    configurando o ambiente

--- veiculo.cpp
+++ veiculo.cpp
@@ -40,39 +40,5 @@ float Aereo::getVelocidadeMax() {
 
 
 
-int main(){
 
-    Veiculo *terr = new Terrestre("VT1");
-    dynamic_cast<Terrestre*>(terr)->setCapacidadeMax(45);
-    terr->mover();
-    cout << "Capacidade maxima de passageiros: " << dynamic_cast<Terrestre*>(terr)->getCapacidadeMax() << endl << endl;
-
-
-    Veiculo *aqua = new Aquatico("VQ1");
-    dynamic_cast<Aquatico*>(aqua)->setCargaMax(12.5);
-    aqua->mover();
-    cout << "Capacidade maxima de carga: " << dynamic_cast<Aquatico*>(aqua)->getCargaMax() << endl << endl ;
-
-
-    Veiculo *aereo = new Aereo("VA1");
-    aereo->mover();
-    dynamic_cast<Aereo*>(aereo)->setVelocidadeMax(1040.5);
-    cout << "Velocidade maxima: " << dynamic_cast<Aereo*>(aereo)->getVelocidadeMax() << endl << endl ;
-
-
-    Veiculo *anfi = new Anfibio("Veiculo Anfibio");
-    anfi->mover();
-
-
-    delete terr;
-    delete aqua;
-    delete aereo;
-    delete anfi;
-
-    //Veiculo v1 ("Moto");
-    //Terrestre terrestre ("Carro");
-    //Aquatico aquatico ("Barco");
-    //Aereo aereo("Aviao");
-    return 0;
-};
 
